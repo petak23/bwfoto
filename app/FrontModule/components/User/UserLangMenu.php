36c3aff2a81a38342d120e7ac4ed26f053eb788c
PV <PV@PV-PC>
2017-09-08 07:59:50 +0200

    Verzia 0.1.3LC2 - oprava vzhľadu

--- app/FrontModule/components/User/UserLangMenu.php
+++ app/FrontModule/components/User/UserLangMenu.php
@@ -9,13 +9,13 @@ use Language_support;
 
 /**
  * Plugin pre zobrazenie ponuky o užívateľovi a jazykoch
- * Posledna zmena(last change): 15.11.2016
+ * Posledna zmena(last change): 08.09.2017
  *
  * @author Ing. Peter VOJTECH ml. <petak23@gmail.com>
- * @copyright  Copyright (c) 2013 - 2016 Ing. Peter VOJTECH ml.
+ * @copyright  Copyright (c) 2013 - 2017 Ing. Peter VOJTECH ml.
  * @license
  * @link       http://petak23.echo-msz.eu
- * @version 1.0.7
+ * @version 1.0.8
  */
 class UserLangMenuControl extends Control {
   /** @var Language_support\User Prednastavene texty pre prihlasovaci form */
@@ -121,7 +121,7 @@ class UserLangMenuControl extends Control {
         'abs_link'=>$baseUrl."/www/adminer/?server=".$hl_m_db_info['host']."&db=".$hl_m_db_info['dbname'], 
         'title'=>'Adminer',
         'target'=>'_blank',
-        'nazov'=>Html::el('img')->src($baseUrl.'/www/ikonky/16/graf_16.png')->alt('Adminer'),
+        'ikonka'  => 'database',
         'class'=>'noajax',
         'data'=>['name'=>'ajax', 'value'=>'false'],
                           ]);
