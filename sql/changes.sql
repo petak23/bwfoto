a4dfae257a78a136c5e7a7a9bc8734ffa827f41e
petak23 <petak23@petak23>
2020-07-14 05:45:34 +0200

    fotogalery 01 - Začiatky fotogalérie vo vue.js.

--- sql/changes.sql
+++ sql/changes.sql
@@ -12,7 +12,7 @@ VALUES ('Front:Search');*/
 /*INSERT INTO `user_permission` (`id_user_roles`, `id_user_resource`, `actions`)
 VALUES ('0', '26', NULL);*/
 
-ALTER TABLE `products`
+/*ALTER TABLE `products`
 CHANGE `change` `change` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE CURRENT_TIMESTAMP COMMENT 'Čas zmeny' AFTER `thumb_file`,
 ADD `saved` tinyint NOT NULL DEFAULT '0' COMMENT 'Ak 1 položka bola korektne nahratá na server';
 
@@ -21,4 +21,4 @@ UPDATE `products` SET `saved` = '1';
 INSERT INTO `udaje` (`id_user_roles`, `id_druh`, `id_udaje_typ`, `nazov`, `text`, `comment`, `separate_settings`)
 SELECT '4', '8', '4', 'product_max_upload_files', '50', 'Max. počet naraz nahrávaných súborov[5,150]', '1'
 FROM `udaje`
-WHERE ((`id` = '32'));
+WHERE ((`id` = '32'));*/
