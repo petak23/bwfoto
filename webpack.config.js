536c635a8cf4e5383d4fe5f7fb6747bd0343c007
petak23 <petak23@petak23>
2020-09-23 17:57:13 +0200

    02 Integrácia webpack-u

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,10 @@ module.exports = {
   devServer: {
     publicPath: '/assets/'
   },
-  entry: [ './www/js/main.js', './www/css/front/main.css' ],
+  entry: {
+    front: ['./www/js/main.js', './www/css/front/main.css' ],
+    admin: ['./www/css/admin/main.css']
+  },
   mode: (process.env.NODE_ENV === 'production') ? 'production' : 'development',
   resolve: {
     extensions: ['*', '.js', '.jsx'],
@@ -15,13 +18,13 @@ module.exports = {
     modules: ['node_modules']
   },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].js', //.[contenthash:8]
     path: path.join(__dirname, 'www', 'assets')
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "style.css"
-    }),
+      filename: "[name].css" //[contenthash:8].
+    })
   ],
   module: {
     rules: [
@@ -39,9 +42,21 @@ module.exports = {
             loader: 'file-loader',
             options: {
               name: '[name].[hash:8].[ext]',
-              outputPath: 'assets/'
+              outputPath: 'images/'
             }
           }  
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[hash:8].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
         ]
       }
     ]
