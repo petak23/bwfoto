c39c8326a4a5a9c84d25300332baa4e3e9c738b8
petak23 <petak23@petak23>
2020-10-03 06:52:58 +0200

    05 Odstránenie chyby

--- app/AdminModule/presenters/ArticlePresenter.php
+++ app/AdminModule/presenters/ArticlePresenter.php
@@ -10,7 +10,7 @@ use PeterVojtech;
 /**
  * Zakladny presenter pre presentery obsluhujuce polozky hlavneho menu v module ADMIN
  * 
- * Posledna zmena(last change): 16.04.2020
+ * Posledna zmena(last change): 03.10.2020
  *
  * Modul: ADMIN
  *
@@ -18,10 +18,11 @@ use PeterVojtech;
  * @copyright  Copyright (c) 2012 - 2020 Ing. Peter VOJTECH ml.
  * @license
  * @link       http://petak23.echo-msz.eu
- * @version 1.4.3
+ * @version 1.4.4
  */
 abstract class ArticlePresenter extends BasePresenter {
   
+  // -- Traity
 //  use PeterVojtech\Clanky\ZobrazKartyPodclankov\zobrazKartyPodclankovTrait;
   use PeterVojtech\Clanky\OdkazNaClanky\odkazNaClankyTrait;
   
@@ -133,7 +134,7 @@ abstract class ArticlePresenter extends BasePresenter {
     $opravnenie_edit = $this->vlastnik($hlm->id_user_main) ? TRUE : (boolean)($hlm->id_hlavne_menu_opravnenie & 2);
     $opravnenie_del = $this->vlastnik($hlm->id_user_main) ? TRUE : (boolean)($hlm->id_hlavne_menu_opravnenie & 4);
     // Test pre pridanie a odkaz: 0 - nemám oprávnenie; 1 - odkaz bude na addpol; 2 - odkaz bude na Clanky:add
-    $druh_opravnenia = $opravnenie_add ? ($this->user->isAllowed($this->name, 'addpol') ? 1 : $this->user->isAllowed($this->name, 'add') ? 2 : 0) : 0;
+    $druh_opravnenia = $opravnenie_add ? ($this->user->isAllowed($this->name, 'addpol') ? 1 : ($this->user->isAllowed($this->name, 'add') ? 2 : 0)) : 0;
     $this->admin_links = [
       "alink" => ["druh_opravnenia" => $druh_opravnenia,
                   "link"    => $druh_opravnenia ? ($druh_opravnenia == 1 ? ['main'=>$this->name.':addpol']
